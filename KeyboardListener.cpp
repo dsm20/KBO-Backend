6ec15e3cb03380085d03223283a16531971621bc
dsm20 <74568547+dsm20@users.noreply.github.com>
2025-10-22 00:12:26 -0400

    key history, modifier tracking
    added key history implementation (using dequeue), a quick map to reference names of modifier keys, and some more console printing. proof of concept stuff

--- KeyboardListener.cpp
+++ KeyboardListener.cpp
@@ -2,35 +2,103 @@
 
 #include <iostream>
 #include <windows.h>
+#include <map>
+#include <deque>
 
 // Compilation: x86_64-w64-mingw32-g++ KeyboardListener.cpp -o KeyboardHookTest.exe -static -luser32 -lgdi32 (need to compile with "static" because of lack of DLL's)
 
 // Global handle for the hook
 HHOOK hHook = NULL;
 
+std::deque<DWORD> g_keyHistory;
+
+// Function to get name of modifier key before I figure out a better way
+// DWORD:keyname -> 162:ctrl, 255:fn, 164:alt, 91:win, 160:shift, 20:caps, 9:tab, 27:esc 
+std::string getSpecialKeyName(DWORD vkCode) {
+    // Mapping from VK Code (DWORD) to Key Name (std::string)
+    static const std::map<DWORD, std::string> specialKeys = {
+        {162, "ctrl"},
+        {255, "fn"},
+        {164, "alt"},
+        {91, "win"},
+        {160, "shift"},
+        {20, "caps"},
+        {9, "tab"},
+        {27, "esc"}
+    };
+
+    // Use map::find to check for the key's existence and retrieve the value
+    std::map<DWORD, std::string>::const_iterator it = specialKeys.find(vkCode);
+
+    if (it != specialKeys.end()) {
+        return it->second; // 'it->second' is the string (key name)
+    } else {
+        return "None";
+    }
+}
+
+
+std::deque<DWORD> updateKeyHistory(DWORD vkCode){
+
+// 2. Add the new key name to the back (most recent).
+    g_keyHistory.push_back(vkCode);
+
+    // 3. Enforce the size limit (5). If the queue is too large, remove the oldest.
+    if (g_keyHistory.size() > 5) {
+        g_keyHistory.pop_front(); // Remove the element from the front (oldest).
+    }
+
+    return g_keyHistory;
+}
+
+
+// Function for state tracking (this will be the "first stop" function and in here separate functions will be called for mod keys and/or sliding window)
+void intakeKey(bool stroke, DWORD vkCode) {
+    // call additional function here to maintain sliding window (queue or stack?) and modifier key state management (separate function)
+    // put the proof of concept print statement here (because we dont need to see key up/key down as is the case with the current print statements)
+
+    // Printing stuff 
+    if (stroke) 
+    {
+        // Update key history with function 
+        updateKeyHistory(vkCode);
+        std::cout << "Key Down:   " << vkCode << "   Modifiers: " << getSpecialKeyName(vkCode) << std::endl;
+        // Print key history after downstroke (most up to date queue of keystrokes including "holds")
+        std::cout << "History: [";
+        bool first = true;
+        for (DWORD code : g_keyHistory) {
+            if (!first) {
+                std::cout << ", ";
+            }
+            std::cout << code;
+            first = false;
+        }
+        std::cout << "]" << std::endl;
+
+    }
+    else
+    {
+        std::cout << "Key Up:   " << vkCode << std::endl;
+
+    }
+
+}
+
 // These functions are called by the LowLevelKeyboardProc
 void oneKeydown(DWORD vkCode) {
 	// Here do some decoding of the vkCode in order to display the key pressed properly
 	// Additionally, figure out way to track states (mainly of modifier keys but also keeping a "sliding window" of characters pressed)
 	// 
-    std::cout << "Key Down: " << vkCode << std::endl;
-    // intakeKey(vkCode)
+    //std::cout << "Key Down: " << vkCode << std::endl;
+    intakeKey(1, vkCode);
 }
 
 void oneKeyup(DWORD vkCode) {
-    std::cout << "Key Up:   " << vkCode << std::endl;
-    // intakeKey(vkCode)
+    //std::cout << "Key Up:   " << vkCode << std::endl;
+    intakeKey(0, vkCode);
 }
 
 
-// Function for state tracking (this will be the "first stop" function and in here separate functions will be called for mod keys and/or sliding window)
-void intakeKey(DWORD vkCode) {
-    // call additional function here to maintain sliding window (queue or stack?) and modifier key state management (separate function)
-    // put the proof of concept print statement here (because we dont need to see key up/key down as is the case with the current print statements)
-
-    // known key:
-    // DWORD:keyname -> 162:ctrl, 255:fn, 164:alt, 91:win, 160:shift, 20:caps, 9:tab, 27:esc 
-}
 
 // planned flow: keyup/down function -> intakeKey() -> process & track -> call respective oneKeyUp/oneKeyDown function from within intakeKey() -> print "state" of the keystroke
 // ex: printed view will show which mod keys are being held, as well as whatever other keys are beind held, as well as the sliding window.
@@ -56,7 +124,6 @@ LRESULT CALLBACK LowLevelKeyboardProc(
             // oneKeydown(vkCode);
             // break;
         case WM_SYSKEYDOWN:
-            // Call your custom function for key down events
             oneKeydown(vkCode);
             break;
 
@@ -64,14 +131,13 @@ LRESULT CALLBACK LowLevelKeyboardProc(
             // oneKeyup(vkCode);
             // break;
         case WM_SYSKEYUP:
-            // Call your custom function for key up events
             oneKeyup(vkCode);
             break;
         }   
     }
 
     // Pass the message to the next hook in the chain
-    return CallNextHookEx(hHook, nCode, wParam, lParam);s
+    return CallNextHookEx(hHook, nCode, wParam, lParam);
 }
 
 // Function to set the hook
@@ -102,7 +168,7 @@ void UnsetGlobalHook() {
     }
 }
 
-// Main
+// MAIN!!!!
 int main() {
     SetGlobalHook();
 
