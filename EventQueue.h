7c04569bb67c17d6d70fc440da703a102336c3cf
dsm20 <martid13@rpi.edu>
2025-11-11 14:54:37 -0500

    Forgot to save

--- EventQueue.h
+++ EventQueue.h
@@ -1,24 +1,29 @@
 // EventQueue.h
 // Custom SPSC queue to store KeyStrokeEvent objects between producer and consumer threads.
 #pragma once
-#include <atomic> // atomic type for head and tail 
+#include <atomic> // atomic type for head and tail
 #include <vector>
 
-template<typename T, size_t N>
-class SpscRing {
+template <typename T, size_t N>
+class SpscRing
+{
 public:
-    bool try_push(const T& v) {
+    bool try_push(const T &v)
+    {
         auto head = _head.load(std::memory_order_relaxed); // maybe fix pointer types later?
         auto next = (head + 1) % N;
-        if (next == _tail.load(std::memory_order_acquire)) return false; // full case
+        if (next == _tail.load(std::memory_order_acquire))
+            return false; // full case
         _buf[head] = v;
         _head.store(next, std::memory_order_release);
         return true;
     }
 
-    bool try_pop(T& out) {
+    bool try_pop(T &out)
+    {
         auto tail = _tail.load(std::memory_order_relaxed);
-        if (tail == _head.load(std::memory_order_acquire)) return false; // empty case
+        if (tail == _head.load(std::memory_order_acquire))
+            return false; // empty case
         out = _buf[tail];
         _tail.store((tail + 1) % N, std::memory_order_release);
         return true;
